1ad3cb72c1a0d47ebcd6e820df2f199231fcc46d
vkalaparthy <kalaparthy.vani@gmail.com>
2020-07-17 16:12:16 -0400

    improved code

--- script.js
+++ script.js
@@ -10,21 +10,31 @@ var resDiv = document.getElementById("resDiv");
 
 var listOfQuestions = [
     {
-        question: "What is 10/2?",
-        answers: ["3", "5", "100", "20"],
-        correctAnswer: "5"
+        question: "HTML stands for?",
+        answers: ["1. Hyper Text Markup Language", "2. High Text Markup Language", "3. Hyper Tabular Markup Language", "4. None of the above"],
+        correctAnswer: 1
     },
     {
-        question: "What is 30/3?",
-        answers: ["3", "5", "10", "1"],
-        correctAnswer: "10"
+        question: "Which of the following tag is used to mark a begining of paragraph?",
+        answers: ["1. &lttd&gt", "2. &ltdiv&gt", "3. &ltbr&gt", "4. &ltp&gt"],
+        correctAnswer: 4
     },
     {
-        question: "How is this homework?",
-        answers: ["Very hard", "Hard", "Easy", "Very easy"],
-        correctAnswer: "Very hard"
+        question: "Javascript is",
+        answers: ["1. A programming language", "2. Compiled code", "3. Scripting language", "4. None of the above"],
+        correctAnswer: 3
+    },
+    {
+        question: "Data types supported in JavaScript",
+        answers: ["1. Boolean", "2. String", "3. Number", "4. All the above"],
+        correctAnswer: 4
+    },
+    {
+        question: "Which of the following is true for Array in JavaScript?",
+        answers: ["1. It can contain only numbers", "2. It can contain only string", "3. A combination of different data type", "4. All the above"],
+        correctAnswer: 4
     }
-];
+]
 
 var cont1 = conts[0];
 var cont2 = conts[1];
@@ -34,7 +44,7 @@ console.log(val);
 
 var questionCount = 0;
 
-var secondsLeft = 6;
+var secondsLeft = 15;
 var lastScore = 0;
 
 function init() {
@@ -51,28 +61,10 @@ function startTimer() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      sendMessage();
+      afterQuiz();
     }
 
   }, 1000);
-}
-
-function sendMessage() {
-    //Add the information to populate High Score
-    console.log ("Time is up!");
-    cont2.setAttribute("style", "display: none;");
-    cont3.setAttribute("style", "display: block;");
-    var newH1 = document.createElement("h1");
-    newH1.textContent = "All Done!!!";
-    cont3.append(newH1);
-    var pElement = document.createElement("h2");
-    //pElement.innerHTML = "<p>Your Score is: "+ lastScore + "</p>";
-    pElement.textContent = "Your Score is: "+lastScore;
-    console.log ("what to display "+pElement.textContent);
-    cont3.appendChild(pElement);
-    var inputBox = document.createElement("input");
-    cont3.appendChild(inputBox);
-    
 }
 
 sButton.addEventListener("click", function() {
@@ -121,13 +113,16 @@ cont2.addEventListener("click", function() {
     event.preventDefault();
     if(event.target.matches("button")) {
         var userAnswer = event.target.textContent;
-        console.log("user answer :"+userAnswer);
-        if (userAnswer === listOfQuestions[questionCount].correctAnswer) {
+        console.log("user answer :"+userAnswer + " char at 0: "+userAnswer.charAt(0));
+        // OR I can use parent id but that is string again!
+        //console.log("parent of event: "+event.target.parentNode.id);
+        if (parseInt(userAnswer.charAt(0)) === listOfQuestions[questionCount].correctAnswer) {
             console.log("Hurray!!!!!");
             lastScore++;
-            displayRes();
+            displayRes(1);
         }
         else {
+            displayRes(0);
             console.log ("Next time!!!!");
         }
         questionCount++;
@@ -137,12 +132,34 @@ cont2.addEventListener("click", function() {
 
 });
 
-function displayRes() {
-    resDiv.innerHTML = "<hr/><p>Correct</p>";
+function displayRes(num) {
+    if (num === 1) {
+        resDiv.innerHTML = "<hr/><p>Correct</p>";
+    } else {
+        resDiv.innerHTML = "<hr><p>Wrong<p>";
+    }
     setTimeout(function(){
         resDiv.innerHTML = "<p></p>";
-        console.log("res " + resDiv.innerHTML);
+        //console.log("res " + resDiv.innerHTML);
     },500);
 };
+
+function afterQuiz() {
+    //Add the information to populate High Score
+    console.log ("Time is up!");
+    cont2.setAttribute("style", "display: none;");
+    cont3.setAttribute("style", "display: block;");
+    var newH1 = document.createElement("h1");
+    newH1.textContent = "All Done!!!";
+    cont3.append(newH1);
+    var h3Element = document.createElement("h3");
+    //pElement.innerHTML = "<p>Your Score is: "+ lastScore + "</p>";
+    h3Element.textContent = "Your Score is: "+lastScore;
+    console.log ("what to display "+h3Element.textContent);
+    cont3.appendChild(h3Element);
+    var inputBox = document.createElement("input");
+    cont3.append("Enter your initials  ", inputBox);
+    
+}
 
 init();
